03c98f55fd2325b487883f1dfd6f8d9e7ad1c255
Paul Fenwick <pjf@perltraining.com.au>
2013-12-25 14:57:33 +1100

    maint-bin: Added `forum-patchlog.pl`, to generate BB friendly patchlogs.
    I was sick of doing this by hand.
    This should really be merged with `export-patches.pl`.

---
+++ maint-bin/forum-patchlog.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/perl
+use v5.10.0;
+use strict;
+use warnings;
+use autodie;
+use IPC::System::Simple qw(capture systemx);
+use Getopt::Std;
+use FindBin qw($Bin);
+use File::Spec;
+
+=head1 NAME
+
+forum-patchlog.pl
+
+=head1 SYNPOSIS
+
+    $ ./forum-patchlog.pl       # Show BB code
+    $ ./forum-patchlog.pl -T    # Show text
+
+=head1 DESCRIPTION
+
+This program shows changes made which have not been merged with the
+'master' branch, or the 'upstream' branch if our current branch is
+already master.
+
+By default, changes are shown with BB code mark-up linking to github.
+The C<-T> switch shows them in plain text format.
+
+If a F<forum-patchlog.exclude> file exists in the same directory as
+this script, then any git commits mentioned in it are ignored. Branch
+merges, pull requests, messages starting with 'maint-bin:', and
+anything wit '#dev' in the commit line are automatically excluded.
+
+=head1 LICENSE
+
+Same as Perl itself.
+
+=head1 AUTHOR
+
+Paul Fenwick (pjf@cpan.org)
+
+=cut
+
+my %opts = (
+    T => 0, # 'text', show without markup
+);
+
+getopts('T',\%opts);
+
+my $REPO_ROOT = "https://github.com/pjf/masterwork-dwarf-fortress";
+
+my $now_branch = capture('git rev-parse --abbrev-ref HEAD');
+chomp($now_branch);
+
+my $parent = ($now_branch eq 'master') ? "upstream" : "master";
+
+my $EXCLUDED_COMMIT_RE = qr{(?:
+      Merge\ branch\ '\w+'
+    | Merge\ pull\ request
+    | maint-bin:
+    | \#dev\b
+)}msx;
+
+# Get our patches
+
+my @patch_log = 
+    grep { not /^\w+ $EXCLUDED_COMMIT_RE/ } capture("git log --oneline $parent..HEAD");
+;
+
+chomp @patch_log;
+
+# Exclude ones we've marked as not to include in the output
+
+my $exclude_file_re;
+
+{
+    open(my $exclude_fh, '<',
+        File::Spec->catdir($Bin, 'forum-patchlog.exclude')
+    );
+
+    my @excluded_commits;
+
+    while (<$exclude_fh>) {
+        chomp;
+
+        next if /^\s*$/;    # Skip blank lines
+        next if /^\s*#/;    # Skip comments;
+
+        s{^(\w+).*}{$1};      # Convert to just git ID
+
+        push(@excluded_commits, $_);
+    }
+
+    # Build regexp
+    $exclude_file_re = join('|',@excluded_commits);
+}
+
+# Print our changes
+
+say "[list]" if not $opts{T};
+
+foreach (@patch_log) {
+    # Ignore anythig in our exclude file
+    next if /$exclude_file_re/;
+
+    if ($opts{T}) {
+        # Text mode, print raw
+        say;
+    }
+    else {
+        # Add BB markup
+        my ($id, $msg) = /^(\w+) (.*)/;
+        say "[li][url=$REPO_ROOT/commit/$id]${msg}[/url][/li]";
+    }
+}
+
+say "[/list]" if not $opts{T};
