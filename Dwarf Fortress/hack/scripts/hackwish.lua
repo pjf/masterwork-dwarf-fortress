55b72d3efc1a76b6966c4b2ce165b817557c8dc0
Paul Fenwick <pjf@perltraining.com.au>
2014-01-16 22:34:47 +1100

    Import Warlock v0.3 alpha from Meph

--- Dwarf Fortress/hack/scripts/hackwish.lua
+++ Dwarf Fortress/hack/scripts/hackwish.lua
@@ -1,140 +1,73 @@
--- Allows for reaction-based wishing.
+-- Allows for script-based wishing.
  
--- Strings be violated here. Be warned.
- 
-function cleanString(str)
-    if not str then return '' end
-    return str:lower():gsub('%W','')
-end
- 
-function desperatelyAttemptToMatchStrings(str1,str2)
-    if not str1 or not str2 then return false end
-    return cleanString(str1):find(cleanString(str2)) or cleanString(str2):find(cleanString(str1))
-end
- 
-function findPlant(str,desparate)
-    --ugly ugly function, but this whole script is ugly
-    if desparate then
-        for k,v in ipairs(df.global.world.raws.plants.all) do
-            if desperatelyAttemptToMatchStrings(str,v.id) or desperatelyAttemptToMatchStrings(str,v.name) then return {v,'plant'} end
-        end
-    else
-        for k,v in ipairs(df.global.world.raws.plants.all) do
-            if cleanString(str)==cleanString(v.id) or cleanString(v.name)==cleanString(str) then return {v,'plant'} end
-        end
+function createItem(mat,itemType,quality,pos)
+    local item=df['item_'..df.item_type[itemType[1]]:lower()..'st']:new() --incredible
+    item.id=df.global.item_next_id
+    df.global.world.items.all:insert('#',item)
+    df.global.item_next_id=df.global.item_next_id+1
+    if itemType[2]~=-1 then
+        item:setSubtype(itemType[2])
     end
-    return {false,nil}
+    item:setMaterial(mat[1])
+    item:setMaterialIndex(mat[2])
+    item:categorize(true)
+    item.flags.removed=true
+    item:setSharpness(1,0)
+    item:setQuality(quality-1)
+    dfhack.items.moveToGround(item,{x=pos.x,y=pos.y,z=pos.z})
 end
  
-function findCreature(str,desparate)
-    if desparate then
-        for k,v in ipairs(df.global.world.raws.creatures.all) do
-            if desperatelyAttemptToMatchStrings(str,v.creature_id) or desperatelyAttemptToMatchStrings(str,v.name[0]) then return {v,'creature'} end
-        end
-    else
-        for k,v in ipairs(df.global.world.raws.creatures.all) do
-            if cleanString(str)==cleanString(v.creature_id) then return {v,'creature'} end --the name equality is handled by the binsearch
-        end
-    end
-    return {false,nil}
+function qualityTable()
+    return {{'None'},
+    {'-Well-crafted-'},
+    {'+Finely-crafted+'},
+    {'*Superior*'},
+    {string.char(240)..'Exceptional'..string.char(240)},
+    {string.char(15)..'Masterwork'..string.char(15)}
+    }
 end
  
-function findMaterialGivenPlainLanguageString(str)
-    --not going to include odd substances :I
-    local str=str:gsub("'",''):gsub('"','')
-    local tokenStr=string.upper(str:gsub(' ','_'))
-    local moddedString=tokenStr:gsub('_',':')
-    for i=1,2 do
-        if i==1 then
-            moddedString='CREATURE:'..moddedString
-        else
-            moddedString='PLANT:'..moddedString
-        end
-        local find=dfhack.matinfo.find(tokenStr) or dfhack.matinfo.find(moddedString)
-        if find then return find.type,find.index end
-    end
-    str=string.lower(str:gsub('_',' ')) --making sure it's all nice for the ugly part
-    --this is the ugly part
-    local utils=require('utils')
-    local foundMatchingObject={}
-    for word in str:gmatch('%a+') do
-        --first, we search for an object, starting with a binsearch followed by a plant search followed by a creature search using a different creature identifier followed by a couple of nasty desparate searches.
-        local binsearchResult={utils.binsearch(df.global.world.raws.creatures.alphabetic,string.lower(word),'name',utils.compare_field_key(0))}
-        foundMatchingObject=foundMatchingObject[1]==true and foundMatchingObject or binsearchResult[2]==true and binsearchResult or findPlant(word) or findCreature(word) or findPlant(word,true) or findCreature(word,true)
-        if foundMatchingObject[1]==true then
-            for k,v in ipairs(foundMatchingObject[1].material) do --then we desperately try to find a material that matches the object.
-                if desperatelyAttemptToMatchStrings(word,v.id) or desperatelyAttemptToMatchStrings(word,v.state_name.Liquid) or desperatelyAttemptToMatchStrings(word,v.state_name.Solid) or 
-                desperatelyAttemptToMatchStrings(str,v.id) or desperatelyAttemptToMatchStrings(str,v.state_name.Liquid) or desperatelyAttemptToMatchStrings(str,v.state_name.Solid) then
-                    if v.heat.melting_point~=60001 then
-                        if foundMatchingObject[2]==true or foundMatchingObject[2]=='creature' then
-                            local find = dfhack.matinfo.find('CREATURE:'..foundMatchingObject[1].creature_id..':'..v.id) --splitting the string doesn't work right now
-                            return find.type,find.index
-                        elseif foundMatchingObject[2]=='plant' then
-                            local find = dfhack.matinfo.find('PLANT:'..foundMatchingObject[1].id..':'..v.id)
-                            return find.type,find.index
-                        else
-                        end
-                    end
-                end
-            end
-        end
-    end
-    return false,false
+local script=require('gui/script')
+ 
+function script.showItemPrompt(text,item_filter,hide_none)
+    require('gui.materials').ItemTypeDialog{
+        text=text,
+        item_filter=item_filter,
+        hide_none=hide_none,
+        on_select=script.mkresume(true),
+        on_cancel=script.mkresume(false),
+        on_close=script.qresume(nil)
+    }:show()
+    
+    return script.wait()
 end
  
-function getItemType(itemdef)
-	return df.item_type[tostring(itemdef._type):sub(16,-4):upper()]
-end
- 
-function findItemGivenPlainLanguageString(str)
-    local str=str:gsub("'",''):gsub('"','')
-    local tokenStr=string.upper(str:gsub(' ','_'))
-	if df.item_type[tokenStr] then return df.item_type[tokenStr],-1 end
-	for k,v in ipairs(df.global.world.raws.itemdefs.all) do
-		if v.id==tokenStr or v.name==str then return getItemType(v),v.subtype end
-	end
-end
- 
-local eventful=require('plugins.eventful')
-eventful.onReactionComplete.putnamWish=function(reaction,unit,input_items,input_reagents,output_items,call_native)
-	local script=require('gui/script')
-	print('Wish trying to run.')
-	if not reaction.code:find('DFHACK_WISH_SET') then return nil end
-	print('Wish running.')
+function hackWish(posOrUnit)
+    local pos = df.unit:is_instance(posOrUnit) and posOrUnit.pos or posOrUnit
     script.start(function()
-        local mattype,matindex,itemtype,itemsubtype,tryAgain
-        repeat
-            local ok,itemString=script.showInputPrompt('Wish','What item do you want from me?',COLOR_LIGHTGREEN)
-            if ok then
-                itemtype,itemsubtype=findItemGivenPlainLanguageString(itemString)
-            end
-            if not itemtype then
-                script.showMessage('Darkpact','Dont try to fool me!',COLOR_LIGHTGREEN)
-                tryAgain=script.showYesNoPrompt('Darkpact','Chose another?',COLOR_LIGHTGREEN)
-            end
-        until not tryAgain or itemtype
-        repeat
-			if not tryAgain then
-            local kk,matString=script.showInputPrompt('Darkpact','What material do you wish?',COLOR_LIGHTGREEN)
-            if kk then
-                mattype,matindex=findMaterialGivenPlainLanguageString(matString)
-            end
-            if not mattype then
-                script.showMessage('Darkpact','Invalid material!',COLOR_LIGHTGREEN)
-                tryAgain=script.showYesNoPrompt('Darkpact','Chose another?',COLOR_LIGHTGREEN)
-            end end --tryAgain also
-        until not tryAgain or mattype
+--        local amountok, amount
+        local itemok,itemtype,itemsubtype=script.showItemPrompt('What item do you want?',function() return true end,true)
+        local matok,mattype,matindex=script.showMaterialPrompt('Wish','And what material should it be made of?')
+        local qualityok,quality=script.showListPrompt('Wish','What quality should it be?',COLOR_LIGHTGREEN,qualityTable())
+--        repeat amountok,amount=script.showInputPrompt('Wish','How many do you want? (numbers only!)',COLOR_LIGHTGREEN) until tonumber(amount)
         if mattype and itemtype then
-            for k,v in ipairs(df.global.world.raws.reactions) do
-                if v.code:find('DFHACK_WISH_GIVE') then
-                    for _,product in ipairs(v.products) do
-                        product.mat_type=mattype
-                        product.mat_index=matindex
-						product.item_type=itemtype
-						product.item_subtype=itemsubtype
-                    end
-                end
-            end
+--            for i=1,tonumber(amount) do
+            createItem({mattype,matindex},{itemtype,itemsubtype},quality,pos)
+--            end
         end
     end)
+end
+ 
+args={...}
+ 
+eventful=require('plugins.eventful')
+ 
+if args[1]~='startup' then 
+    local posOrUnit=#args<1 and dfhack.gui.getSelectedUnit(true) and dfhack.gui.getSelectedUnit(true) or #args<1 and df.global.cursor or #args<3 and df.unit.find(args[1]) or {x=args[1],y=args[2],z=args[3]}
+    hackWish(posOrUnit)
+else
+    eventful.onReactionComplete.hackWishP=function(reaction,unit,input_items,input_reagents,output_items,call_native)
+        if not reaction.code:find('DFHACK_WISH') then return nil end
+        hackWish(unit)
+    end
 end
